7e73752bb3a9bae20997a96d161cbb6b00c4e0b6
Pedro <pedroshalommrn@hotmail.com>
2023-05-25 14:19:09 -0300

    reformulando footer

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Footer.css";
+
+function Footer() {
+    return (
+
+        <>
+            <div className="main-footer">
+                <div className="row">
+                    <div class="medias-socias">
+                        <a className="face" href="face"> <i class="fa fa-facebook"></i> </a>
+                        <a className="ins" href="https://www.instagram.com/artecrochenatal/"> <i class="fa fa-instagram"></i> </a>
+                        <a className="whatts" href="https://api.whatsapp.com/send?phone=5584999096035&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos."> <i class="fa fa-whatsapp"></i> </a>
+                        {/* <a href="#"> <i class="fa fa-twitter"></i> </a> */}
+                    </div>
+                    <p className="col-sm">
+                        Arte Crochê
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
 import '../../index.css'
+import Footer from "../Footer";
 
 export const Home = () => {
   return (
-    <><div>
+    <>
+    <div>
       <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div className='hero-container'>
           <p className='p1'>Especialista em lembrancinhas, kit higiene e bolsas</p>
@@ -19,23 +21,8 @@ export const Home = () => {
 
 
 
+    <Footer/>
     </div>
-    <footer>
-        <div class="container-footer">
-          <div class="row-footer">
-            <div class="footer-col">
-              <div class="medias-socias">
-                <a href="face"> <i class="fa fa-facebook"></i> </a>
-                <a href="https://www.instagram.com/artecrochenatal/"> <i class="fa fa-instagram"></i> </a>
-                <a href="https://api.whatsapp.com/send?phone=5584999096035&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos."> <i class="fa fa-whatsapp"></i> </a>
-                {/* <a href="#"> <i class="fa fa-twitter"></i> </a> */}
-              </div>
-              <p className="finish">Arte Crochê</p>
-
-            </div>
-          </div>
-        </div>
-      </footer>
       </>
   );
 };
