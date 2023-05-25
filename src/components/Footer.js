519da89bff10e9917a5912f0c9746e4ba2451f41
Pedro <pedroshalommrn@hotmail.com>
2023-05-25 15:09:37 -0300

    reformulando footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,12 +5,22 @@ function Footer() {
     return (
 
         <>
-        <div class="medias-socias">
-                        <a className="face" href="face"> <i class="fa fa-facebook"></i> </a>
-                        <a className="ins" href="https://www.instagram.com/artecrochenatal/"> <i class="fa fa-instagram"></i> </a>
-                        <a className="whatts" href="https://api.whatsapp.com/send?phone=5584999096035&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos."> <i class="fa fa-whatsapp"></i> </a>
-                        {/* <a href="#"> <i class="fa fa-twitter"></i> </a> */}
-                    </div>
+             <footer>
+        <div class="container-footer">
+          <div class="row-footer">
+            <div class="footer-col">
+              <div class="medias-socias">
+                <a href="face"> <i class="fa fa-facebook"></i> </a>
+                <a href="https://www.instagram.com/artecrochenatal/"> <i class="fa fa-instagram"></i> </a>
+                <a href="https://api.whatsapp.com/send?phone=5584999096035&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos."> <i class="fa fa-whatsapp"></i> </a>
+                {/* <a href="#"> <i class="fa fa-twitter"></i> </a> */}
+              </div>
+              <p className="finish">Arte Crochê</p>
+
+            </div>
+          </div>
+        </div>
+      </footer>
         </>
     )
 }
