1140c1064a9e47ef70839949e86d8bb6b9bdb29c
Pedro <pedroshalommrn@hotmail.com>
2023-05-25 15:11:10 -0300

    mudando o footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,9 +10,9 @@ function Footer() {
           <div class="row-footer">
             <div class="footer-col">
               <div class="medias-socias">
-                <a href="face"> <i class="fa fa-facebook"></i> </a>
-                <a href="https://www.instagram.com/artecrochenatal/"> <i class="fa fa-instagram"></i> </a>
-                <a href="https://api.whatsapp.com/send?phone=5584999096035&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos."> <i class="fa fa-whatsapp"></i> </a>
+                <a className="face" href="face"> <i class="fa fa-facebook"></i> </a>
+                <a  className="ins" href="https://www.instagram.com/artecrochenatal/"> <i class="fa fa-instagram"></i> </a>
+                <a className="whatts" href="https://api.whatsapp.com/send?phone=5584999096035&text=Ol%C3%A1,%20gostaria%20de%20saber%20mais%20sobre%20os%20produtos."> <i class="fa fa-whatsapp"></i> </a>
                 {/* <a href="#"> <i class="fa fa-twitter"></i> </a> */}
               </div>
               <p className="finish">Arte Crochê</p>
