354b80fc46d1b5f56c3f876d039d2e2b47e34c46
Pedro <pedroshalommrn@hotmail.com>
2023-05-25 14:23:14 -0300

    reformulando footer

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,21 +5,24 @@ import Footer from "../Footer";
 export const Home = () => {
   return (
     <>
-    <div>
-      <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-        <div className='hero-container'>
-          <p className='p1'>Especialista em lembrancinhas, kit higiene e bolsas</p>
-          <p className='p2'>Enviamos para todo o Brasil</p>
-        </div>
-        <div class="carousel-inner">
-          <div class="carousel-item">
-            <img className="img2" src="/assets/images/products/11.png" class="d-block w-100" alt="IPhone" height="400px" />
-            <hr />
+    <>
+      <div>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+          <div className='hero-container'>
+            <p className='p1'>Especialista em lembrancinhas, kit higiene e bolsas</p>
+            <p className='p2'>Enviamos para todo o Brasil</p>
+          </div>
+          <div class="carousel-inner">
+            <div class="carousel-item">
+              <img className="img2" src="/assets/images/products/11.png" class="d-block w-100" alt="IPhone" height="400px" />
+              <hr />
+            </div>
           </div>
         </div>
       </div>
-    </div>
-      <Footer/>
-      </>
+    </><div>
+
+        <Footer />
+      </div></>
   );
 };
